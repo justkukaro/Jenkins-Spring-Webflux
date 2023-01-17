becb829d42dfd8fe98bfdec31c3b7f442eede0b2
Coeat.k <coeat.k@kakaocorp.com>
2023-01-18 08:57:01 +0900

    feat:init project

---
+++ src/main/java/com/example/jenkinsspringwebflux/JenkinsSpringWebfluxApplication.java
@@ -0,0 +1,13 @@
+package com.example.jenkinsspringwebflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsSpringWebfluxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsSpringWebfluxApplication.class, args);
+    }
+
+}
